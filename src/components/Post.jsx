eada197abc9c5d9294bb4b5a82a02577cce0112e
Marcko <marco.ramireze@gmail.com>
2023-09-11 23:51:02 -0600

    Update - Card design

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,20 +1,19 @@
-import imagenLocal from '../assets/img/premier-league-intro.png';
-
-export function Post({likes, autor, imagen, comments, date}) {
+export function Post({likes, autor, image, comments, date, text}) {
   return (
     <div className="container mt-2">
-        <div className="card" >
-            {/* <img src={ imagenLocal } className="card-img-top" alt="Imagen" /> */}
-            <img src={ imagen } className="card-img-top" alt="Imagen" style={{minHeight:"15em", maxHeight:"15em"}}/>
+        <div className="card h-100">
+            <img src={ image } className="card-img-top" alt="Imagen" style={{minHeight:"15em", maxHeight:"15em"}}/>
             <div className="card-body">
                 <div className="d-flex justify-content-end">
                     <h5 className="postDate me-auto">{ date }</h5>
                     <button className='btn btn-danger'><i className="fa-solid fa-heart"/>{likes || 1}k</button>
                 </div>
                 <h5 className="userInfo">{autor}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">{text || "..."}</p>
                 
-                <h6 className="card-subtitle comment"><i className="fa-regular fa-message me-2 comment"/>Comments ({ comments })</h6>
+            </div>
+            <div class="card-footer">
+                <h6 className="card-subtitle comment"><i class="bi bi-chat-right me-2 comment"></i>Comments ({ comments })</h6>
             </div>
         </div>
     </div>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,11 +4,11 @@ import { Post } from './Post'
 export const PostList = () => {
   return (
     <div className="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
-        <Post date="1 min ago" likes="20" comments="3" imagen="https://picsum.photos/300/250" autor="@Raphael"/>
-        <Post date="2 min ago" likes="10" comments="4" imagen="https://picsum.photos/301/250" autor="@Leonardo"/>
-        <Post date="7 min ago" likes="30" comments="10" imagen="https://picsum.photos/320/250" autor="@Michelangelo"/>
-        <Post date="9 min ago" likes="40" comments="0" imagen="https://picsum.photos/310/250" autor="@Donatello"/>
-        <Post date="20 min ago" likes="50" comments="9" imagen="https://picsum.photos/305/250" autor="@April"/>
+        <Post date="1 min ago" likes="20" comments="3" image="https://picsum.photos/300/250" autor="@raphael" text="Should he write it down? That was the question running through his mind. He couldn't believe what had just happened and he knew nobody else would believe him as well."/>
+        <Post date="2 min ago" likes="10" comments="4" image="https://picsum.photos/301/250" autor="@leonardo" text="Even if he documented what had happened by writing it down, he still didn't believe anyone would still believe it. So the question remained. Was it be worth it."/>
+        <Post date="7 min ago" likes="30" comments="10" image="https://picsum.photos/320/250" autor="@michelangelo" text="It was just a burger. Why couldn't she understand that? She knew he'd completely changed his life around her eating habits, so why couldn't she give him a break this one time? Yes, he had promised her and yes, he had broken that promise, but still in his mind, all it had been was just a burger."/>
+        <Post date="9 min ago" likes="40" comments="0" image="https://picsum.photos/310/250" autor="@donatello" text="She wasn't even supposed to have found out. Yes, he had promised her and yes, he had broken that promise, but still in his mind, all it had been was just a burger."/>
+        <Post date="20 min ago" likes="50" comments="9" image="https://picsum.photos/305/250" autor="@april" text="She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day"/>
     </div>
   )
 }
